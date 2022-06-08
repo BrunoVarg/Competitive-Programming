89f597a8adce040a456fabbca285f18a308a131f
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-06-08 09:05:58 -0300

    Update divisores.cpp

--- Materiais/divisores.cpp
+++ Materiais/divisores.cpp
@@ -1,24 +1,15 @@
+// DIVISORES
+
 #include <bits/stdc++.h>
 using namespace std;
-// g++ -std=c++17 -O2 -Wall run.cpp -o run
 
-#define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define all(x) begin(x), end(x)
-#define input(x) for (auto &it : x) cin >> it
-#define print(x) for (auto &it : x) cout << it << ' '
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> pii;
 const int MAX = 1e5+7;
 const ll MOD = 1e9+7;
 const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-//----------------------------------//
-//         Fé que o AC vem          //
-//----------------------------------//
+
 ll gcd(ll a, ll b){
     return b ? gcd(b, a % b) : a;
 }
