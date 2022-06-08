ecd91c2fe748b9530d7ffc2bc418f1fa2c14c75e
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-06-08 08:58:55 -0300

    Update buscabinaria.cpp

--- Materiais/buscabinaria.cpp
+++ Materiais/buscabinaria.cpp
@@ -1,3 +1,5 @@
+// BUSCA BINÁRIA
+
 bool check(int valor, int x) {
     return valor <= x;
 }
@@ -12,3 +14,31 @@ int binary_search(int a, int b, int x){
     }
     return l;
 }
+
+// Valor mínimo
+int bb_menor(int a, int b){
+    int l = a;
+    int r = b;
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (check(mid)) r = mid;
+        else l = mid + 1;
+    }
+    // a resposta fica em l
+    return l
+}
+// lower_bound(vetor.begin(), vetor.end(), x) - vetor.begin(); 
+
+// Valor máximo
+int bb_maior(int a, int b){
+    int l = a;
+    int r = b;
+    while (l < r) {
+        int mid = (l + r) / 2;
+        if (!check(mid)) r = mid; // check vira !check
+        else l = mid + 1;
+    }
+    // a resposta fica em l-1
+}
+// upper_bound(vetor.begin(), vetor.end(), x) - vetor.begin(); 
+
