8fe2281fbfe53e19a66d816518886a6aa9fe3cb7
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-06-08 09:00:31 -0300

    Update buscabinariadouble.cpp

--- Materiais/buscabinariadouble.cpp
+++ Materiais/buscabinariadouble.cpp
@@ -1,29 +1,12 @@
+// BUSCA BINÁRIA USANDO DOUBLE
+
 #include <bits/stdc++.h>
 using namespace std;
-//g++ -std=c++17 -O2 -Wall run.cpp -o run
  
-#define endl "\n"
-#define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define all(x) x.begin(), x.end()
-#define input(x) for (auto &it : x) cin >> it
-#define print(x) for (auto &it : x) cout << it << ' '; cout<<endl;
-#define dbg(msg, x) cout << msg << " = " << x << endl
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define TETO(a, b) ((a) + (b-1))/(b)
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pii;
-const float PI = 3.1415;
-const ll MOD = 32768;
 const int MAX = 1e5+1;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
 const double EPS = 0.0000001;
-//----------------------------------//
-//         Fé que o AC vem          //
-//----------------------------------//
+
 vector<int> v(100001);
 int n;
 ll check(double x){
@@ -55,7 +38,6 @@ int main(){ sws;
     }
 
     cout<<fixed<<setprecision(7)<<mid<<endl;
-
     
     return 0;
 }
