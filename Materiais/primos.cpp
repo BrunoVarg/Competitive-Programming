bd755c19f6a98626f3bf3a1d67f493916d095686
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-06-08 09:15:13 -0300

    Update primos.cpp

--- Materiais/primos.cpp
+++ Materiais/primos.cpp
@@ -1,12 +1,15 @@
+// PRIMALIDADE
+
 #include <bits/stdc++.h>
 using namespace std;
-// g++ -std=c++17 -O2 -Wall run.cpp -o run
 
 #define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 typedef long long ll;
 typedef vector<int> vi;
 const int MAX = 1e5+7;
 const ll MOD = 1e9+7;
+
+
 void crivo(){
     vi crivo(MAX, 1);
     for(int i=2; i*i<=MAX; i++){
