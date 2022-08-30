e4b63f647c55e4241297052512339b920e7387da
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-08-29 23:39:02 -0300

    Update divisibilidade.cpp

--- Materiais/Math/divisibilidade.cpp
+++ Materiais/Math/divisibilidade.cpp
@@ -5,5 +5,5 @@
 // 6 -> se é par e a soma dos algarismos é divisível por 3
 // 7 -> se o dobro do ultimo algarismo subtraido do numero sem o ultimo algarimos é divisivel por 7
 // 8 -> se os 3 ultimos algarismos é divisivel por 8
-// 9 -> se a soma dos algarimos é divisivel por 9
-// 10 -> se o ultimo algarimo é 0
+// 9 -> se a soma dos algarismos é divisivel por 9
+// 10 -> se o ultimo algarismo é 0
