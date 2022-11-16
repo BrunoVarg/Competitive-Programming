589031384dbe4b81e079ebb70092629a726627ff
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-11-16 08:59:23 -0300

    Update dsu.cpp

--- Materiais/ED/dsu.cpp
+++ Materiais/ED/dsu.cpp
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1e5+10;
-
-int parent[MAX];
-int sz[MAX];
-
-void make(int v){
-    parent[v] = v;
-    sz[v] = 1;
-}
-
-int find(int v){
-    if (v == parent[v])
-        return v;
-    return parent[v] = find(parent[v]);
-}
-
-void _union(int a, int b){
-    a = find(a);
-    b = find(b);
-
-    if(sz[b]>sz[a]) swap(a,b);
-    if (a != b){
-        sz[a] += sz[b];
-        parent[b] = a;
+// Complexidade 
+// build : O(N)
+// find : O(logN)
+class DSU{
+    vector<int> parent, sz;
+    public:
+    void make(int v){
+        parent[v] = v;
+        sz[v] = 1;
     }
-}
+
+    int find(int v){
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    void union(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(sz[b]>sz[a]) swap(a,b);
+        if (a != b){
+            sz[a] += sz[b];
+            parent[b] = a;
+        }
+    }
+    
+    bool same(int a, int b){
+        int a = find(a), b = find(b);
+        return a == b;
+    }
+    
+    DSU(int n): parent(n+1), sz(n+1){
+        for(int i=1; i<=n; i++) make(i);
+    }
+};
+
 
 int main(){
-
+    DSU dsu(10);
     return 0;
 }
