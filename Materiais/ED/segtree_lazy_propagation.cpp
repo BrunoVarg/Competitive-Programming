cbd56537a2cdc02c66e70ccf2b222de1f4a4e695
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2022-11-20 22:27:49 -0300

    arrumando lazy increasing

--- Materiais/ED/segtree_lazy_propagation.cpp
+++ Materiais/ED/segtree_lazy_propagation.cpp
@@ -1,63 +1,56 @@
-#include <bits/stdc++.h>
-#define ll long long
-
-using namespace std;
-
-const int MAX = 1e5; // tamanho maximo do vetor
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-
-// End Template //
-
-vector<ll> lazy(4*MAX, 0);
-ll tree[4*MAX], vet[MAX];
-int N;
-
-ll merge(ll a, ll b){
+const int MAX = 2e5+20;
+vector<int> lazy(4*MAX, 0);
+int tree[4*MAX], vet[MAX];
+int n;
+ 
+int merge(int a, int b){
     return a + b;
 }
-
-void build(int l=0, int r=N-1, int no=1){
+ 
+void build(int l=0, int r=n-1, int no=1){
     if(l==r){
         tree[no] = vet[l];
+        lazy[no] = 0;
         return;
     }
     int mid = (l+r)/2;
     build(l, mid, 2*no);
     build(mid+1, r, 2*no+1);
-
+ 
     tree[no] = merge(tree[2*no], tree[2*no+1]);
 }
-
+ 
 void prop(int l, int r, int no){
     if(lazy[no]!=0){
-        tree[no] = (r-l+1)*lazy[no]; //acho que na verdade é += em vez de =
+        tree[no] += (r-l+1)*lazy[no];
         if(l!=r){
-            lazy[2*no] = lazy[2*no+1] = lazy[no];
+            lazy[2*no] += lazy[no];
+            lazy[2*no+1] += lazy[no];
         }
         lazy[no] = 0;
     }
 }
-
-void update(int A, int B, int x, int l=0, int r=N-1, int no=1){
+ 
+void update(int A, int B, int x, int l=0, int r=n-1, int no=1){
     prop(l, r, no);
     // caso 1
     if(B<l or r<A) return;
     // caso 2
     if(A<=l and r<=B){
-        lazy[no] = x;
+        lazy[no] += x;
         prop(l, r, no);
         return;
     }
     // caso 3
     int mid = (l+r)/2;
-
+ 
     update(A, B, x, l, mid, 2*no);
     update(A, B, x, mid+1, r, 2*no+1);
     
     tree[no] = merge(tree[2*no], tree[2*no+1]);
 }
-
-ll query(int A, int B, int l=0, int r=N-1, int no=1){
+ 
+int query(int A, int B, int l=0, int r=n-1, int no=1){
     prop(l, r, no);
     // caso 1
     if(B<l or r<A) return 0;
@@ -65,47 +58,32 @@ ll query(int A, int B, int l=0, int r=N-1, int no=1){
     if(A<=l and r<=B) return tree[no];
     // caso 3
     int mid = (l+r)/2;
-
+ 
     return merge(query(A, B, l, mid, 2*no),
                  query(A, B, mid+1, r, 2*no+1));
 }
-
-
-int32_t main()
-{
-
-    int Q, opt, a, b, l, r, k, idx;
-    cin >> N >> Q;
-    vector<int> vaux(N);
-    for(int i=0;i<N;i++){
-        cin >> vaux[i];
-        vet[i] = vaux[i];
-    }
-    for(int i=0; i<N; i++){
-        if(i==0)vet[i] = vaux[i];
-        else vet[i] = vet[i-1] + vaux[i];
-    }
+ 
+ 
+int32_t main(){sws;
+	int q;
+	cin>>n>>q;
+ 
+    for(int i=0;i<n;i++) cin >> vet[i];
     build();
-
-    for(int i=0;i<Q;i++){
-        cin >> opt;
-        if(opt==1){ // update
-            cin >> idx >> k;
-            idx--;
-            int soma = -vaux[idx] + k;
-
-            vaux[idx] = k;
-            update(idx, N-1, soma);
-        }else{ // query
-            cin >> l >> r;
-            l--;r--; // indice indexado em 0
-            cout << query(l, r) << endl;
-        }
-    }
-    for(int i=0; i<N; i++){
-        cout<<vet[i]<<" ";
-    }
-    cout<<endl;
-
+ 
+	while(q--){
+		int opt; cin>>opt;
+		if(opt == 1){
+			int l, r, u;
+			cin>>l>>r>>u;
+			l--; r--;
+			update(l, r, u);
+		}else{
+			int k; cin>>k;
+			k--;
+			cout<< query(k, k) <<endl;
+		}
+	}
+ 
     return 0;
 }
