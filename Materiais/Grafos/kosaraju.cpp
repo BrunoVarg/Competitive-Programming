8e9e0d6d3a5ad886e4c9abbb4841dcf5761dd464
BrunoVarg <brunovargas7899@gmail.com>
2023-03-07 16:07:18 -0300

    adicionando coisas de grafos

--- Materiais/Grafos/kosaraju.cpp
+++ Materiais/Grafos/kosaraju.cpp
@@ -1,51 +1,69 @@
 // Acha componentes fortemente conexas
 // ou seja, que tem caminho entre todos os pares de vertices
 // O(n+m)
-bool visited[MAX];
-int n;
-vector<int> adj[MAX];
-vector<int> adj2[MAX];// grafo inverso
-stack<int> st;
-int conex[MAX]; // conexo de cada vertice
-void dfs(int u){
-    visited[u] = true;
-    for(auto v : adj[u]){
-        if(!visited[v]) dfs(v);
-    }
-    st.push(u);
-}
-// strongly connected component
-void scc(int u, int c){
-    // cout<<u<<" ";
-    visited[u] = true;
-    conex[u] = c;
-    for(auto v : adj2[u]){
-        if(!visited[v]) scc(v, c);
-    }
-}
 
-void kosaraju(){
-    for(int i=1; i<=n; i++)visited[i]=false;
-    for(int i=1; i<=n; i++) if(!visited[i]) dfs(i);  
-
-    for(int i=1; i<=n; i++)visited[i]=false;
-    while(!st.empty()){
-        int u = st.top(); st.pop();
-        if(!visited[u]) scc(u, u);
+// SCC from BenQ
+class SCC{
+    int N;
+    public:
+    vector<int> adj[MAX], radj[MAX];
+    stack<int> st;
+    vector<bool> visited;
+    // todas as componentes
+    vector<int> comps;
+    // componente do vertice
+    vector<int> comp;
+    
+    void inp(int x, int y) {
+        adj[x].pb(y), radj[y].pb(x);
     }
-}
-int main(){
-    int m;
+    void dfs(int u){
+        visited[u] = true;
+        for(auto v: adj[u]) if(!visited[v]) dfs(v);
+        st.push(u);
+    }
+    void dfs2(int u, int c){
+        comp[u] = c;
+        for(auto v: radj[u]) if(comp[v] == -1) dfs2(v, c);
+    }
+    void gen() {
+        for(int i=1; i<=N; i++) if(!visited[i]) dfs(i);
+        while(!st.empty()){
+            int u = st.top(); st.pop();
+            if(comp[u] == -1){
+                dfs2(u, u);
+                comps.pb(u);
+            }
+        }
+    }
+    SCC(int n){
+        N = n+1;
+        comp.assign(N, -1);
+        visited.assign(N, false);
+    }
+};
+
+int32_t main(){ sws; 
+    int n, m;
     cin>>n>>m;
-
+    SCC scc(n);
     for(int i=0; i<m; i++){
         int a, b;
         cin>>a>>b;
-        adj[a].pb(b);
-        adj2[b].pb(a);
+        scc.inp(a, b);
     }
-
-    kosaraju();
-
+    int comp=0;
+    vector<int> ans(n+1);
+    scc.gen();
+    cout<<scc.comps.size()<<endl;
+    for(int i=1; i<=n; i++){
+        if(!ans[scc.comp[i]]){
+            ans[scc.comp[i]] = ++comp;
+        }
+    }
+    for(int i=1; i<=n; i++){
+        cout<<ans[scc.comp[i]]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
--- Materiais/Grafos/lca.cpp
+++ Materiais/Grafos/lca.cpp
@@ -11,68 +11,65 @@ using namespace std;
 const int SIZE = 2e5+5;
 const int LOG = 30; // log2(SIZE)+1;
 int depth[SIZE];
-//ll weight[SIZE];
-int pai[SIZE];
 vector<pair<int,int>> adj[SIZE];
 int up[SIZE][LOG];
 
-// 
 void dfs(int u, int p) {
-	for(auto edge : adj[u]) {
+    for(auto edge : adj[u]) {
         int v, w;
         tie(v, w) = edge;
-	    if(v != p){
-	        up[v][0] = u;
-	        //weight[v] = weight[u] + w;
-	        depth[v] = depth[u] + 1;
-	        for(int i=1; i<LOG; i++){
-    			up[v][i] = up[ up[v][i-1] ][i-1];
-    		}
-    		dfs(v, u);
-	    }
-	}
+        if(v != p){
+            up[v][0] = u;
+            //weight[v] = weight[u] + w;
+            depth[v] = depth[u] + 1;
+            for(int i=1; i<LOG; i++){
+                up[v][i] = up[ up[v][i-1] ][i-1];
+            }
+            dfs(v, u);
+        }
+    }
 }
 
 int lca(int a, int b) {
-	if(depth[a] < depth[b]) swap(a,b);
-	int k = depth[a] - depth[b];
-	for(int i=0; i<LOG; i++){
-		if(k & (1 << i)){
-			a = up[a][i];
-		}
-	}
-	if(a == b) return a;
-	for (int i = LOG-1; i >= 0; i--) {
-		if(up[a][i] != up[b][i]) {
-			a = up[a][i];
-			b = up[b][i];
-		}
-	}
-	return up[a][0];
+    if(depth[a] < depth[b]) swap(a,b);
+    int k = depth[a] - depth[b];
+    for(int i=0; i<LOG; i++){
+        if(k & (1 << i)){
+            a = up[a][i];
+        }
+    }
+    if(a == b) return a;
+    for (int i = LOG-1; i >= 0; i--) {
+        if(up[a][i] != up[b][i]) {
+            a = up[a][i];
+            b = up[b][i];
+        }
+    }
+    return up[a][0];
 }
 
 ll dist(int u, int v){
-	return depth[u] + depth[v] - 2*depth[lca(u,v)];
-	// return weight[u] + weight[v] -2*weight[lca(u,v)];
+    return depth[u] + depth[v] - 2*depth[lca(u,v)];
+    // return weight[u] + weight[v] -2*weight[lca(u,v)];
 }
 
 int main() {
-	int n; cin>>n;
+    int n; cin>>n;
 
-	for(int i=0; i<n-1; i++){
-		int x, y, z;
-		cin>>x>>y>>z;
-		adj[x].push_back({y, z});
-		adj[y].push_back({x, z});
-	}
-	// raiz
-	dfs(1, 0);
+    for(int i=0; i<n-1; i++){
+        int x, y, z;
+        cin>>x>>y>>z;
+        adj[x].push_back({y, z});
+        adj[y].push_back({x, z});
+    }
+    // raiz
+    dfs(1, 0);
 	
-	int q; cin>>q;
-	while(q--){
-		int a, b, c;
-		cin>>a>>b>>c;
-		long long x = dist(a, b) + dist(b, c);
-		cout<<x<<endl;
-	}
+    int q; cin>>q;
+    while(q--){
+        int a, b, c;
+        cin>>a>>b>>c;
+        long long x = dist(a, b) + dist(b, c);
+        cout<<x<<endl;
+    }
 }
